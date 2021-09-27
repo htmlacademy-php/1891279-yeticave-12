ab92579361af5c9a721473c027016942c76b40b5
Савелий <savel97@bk.ru>
2021-09-27 21:54:29 +0300

    Исправления

--- schema.sql
+++ schema.sql
@@ -1,46 +1,81 @@
-CREATE DATABASE yeticave
-	DEFAULT CHARACTER SET utf8
-	DEFAULT COLLATE UTF8_GENERAL_CI;
+-- --------------------------------------------------------
+-- Хост:                         127.0.0.1
+-- Версия сервера:               5.7.33 - MySQL Community Server (GPL)
+-- Операционная система:         Win64
+-- HeidiSQL Версия:              11.3.0.6295
+-- --------------------------------------------------------
 
-USE YETICAVE;
-CREATE TABLE cats (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	cat_name VARCHAR(12),
-	cat_code VARCHAR(10)
-);
-CREATE UNIQUE INDEX cat_name ON cats(cat_name);
-CREATE UNIQUE INDEX cat_code ON cats(cat_code);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE lots (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	creation_date TIMESTAMP,
-	lot_name CHAR,
-	description CHAR,
-	image CHAR,
-	start_price INT,
-	exp_date TIMESTAMP,
-	bet_range INT,
-	creator_id INT,
-	winner_id INT,
-	cat_id TINYINT
-);
 
-CREATE TABLE bets (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	bet_date TIMESTAMP,
-	price INT,
-	user_id INT,
-	lot_id INT
-);
+-- Дамп структуры базы данных yeticave
+CREATE DATABASE IF NOT EXISTS `yeticave` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `yeticave`;
 
-CREATE TABLE users (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	reg_date TIMESTAMP,
-	email CHAR,
-	user_name CHAR,
-	pass CHAR,
-	contacts CHAR,
-	lot_id INT,
-	bet_id INT
-);
-CREATE UNIQUE INDEX email ON users(email);
+-- Дамп структуры для таблица yeticave.bets
+CREATE TABLE IF NOT EXISTS `bets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bet_date` timestamp NULL DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `lot_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Экспортируемые данные не выделены.
+
+-- Дамп структуры для таблица yeticave.cats
+CREATE TABLE IF NOT EXISTS `cats` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cat_name` varchar(12) DEFAULT NULL,
+  `cat_code` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `cat_name` (`cat_name`),
+  UNIQUE KEY `cat_code` (`cat_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Экспортируемые данные не выделены.
+
+-- Дамп структуры для таблица yeticave.lots
+CREATE TABLE IF NOT EXISTS `lots` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `creation_date` timestamp NULL DEFAULT NULL,
+  `lot_name` varchar(100) DEFAULT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  `image` varchar(100) DEFAULT NULL,
+  `start_price` int(11) DEFAULT NULL,
+  `exp_date` timestamp NULL DEFAULT NULL,
+  `bet_range` int(11) DEFAULT NULL,
+  `creator_id` int(11) DEFAULT NULL,
+  `winner_id` int(11) DEFAULT NULL,
+  `cat_id` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Экспортируемые данные не выделены.
+
+-- Дамп структуры для таблица yeticave.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `reg_date` timestamp NULL DEFAULT NULL,
+  `email` varchar(20) DEFAULT NULL,
+  `user_name` varchar(50) DEFAULT NULL,
+  `pass` varchar(50) DEFAULT NULL,
+  `contacts` varchar(50) DEFAULT NULL,
+  `lot_id` int(11) DEFAULT NULL,
+  `bet_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Экспортируемые данные не выделены.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
