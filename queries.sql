1eb6c0a4f754f1638e8f6ba294ed91698b5c6abc
SavelyOlegych <savel97@bk.ru>
2021-10-10 12:56:47 +0300

    Исправления

--- queries.sql
+++ queries.sql
@@ -1,37 +1,42 @@
 USE yeticave;
 
-INSERT INTO categories SET category_name = 'Доски и лыжи', category_code = 'boards';
-INSERT INTO categories SET category_name = 'Крепления', category_code = 'attachment';
-INSERT INTO categories SET category_name = 'Ботинки', category_code = 'boots';
-INSERT INTO categories SET category_name = 'Одежда', category_code = 'clothing';
-INSERT INTO categories SET category_name = 'Инструменты', category_code = 'tools';
-INSERT INTO categories SET category_name = 'Разное', category_code = 'other';
+INSERT INTO categories (category_name, category_code)
+VALUES
+	('Доски и лыжи', 'boards'),
+	('Крепления', 'attachment'),
+	('Ботинки', 'boots'),
+	('Одежда', 'clothing'),
+	('Инструменты', 'tools'),
+	('Разное', 'other');
 
-INSERT INTO lots SET creation_date = '2021-09-27 00:00', lot_name = '2014 Rossignol District Snowboard',
-	image = 'img/lot-1.jpg', start_price = 10999, exp_date = '2021-10-10 00:00', creator_id = 1, category_id = 1;
-INSERT INTO lots SET creation_date = '2021-09-27 03:00', lot_name = 'DC Ply Mens 2016/2017 Snowboard',
-	image = 'img/lot-2.jpg', start_price = 159999, exp_date = '2021-10-11 00:00', creator_id = 1, category_id = 1;
-INSERT INTO lots SET creation_date = '2021-09-26 00:00', lot_name = 'Крепления Union Contact Pro 2015 года размер L/XL',
-	image = 'img/lot-3.jpg', start_price = 8000, exp_date = '2021-10-12 15:00', creator_id = 2, category_id = 2;
-INSERT INTO lots SET creation_date = '2021-09-25 00:00', lot_name = 'Ботинки для сноуборда DC Mutiny Charocal',
-	image = 'img/lot-4.jpg', start_price = 10999, exp_date = '2021-10-13 00:00', creator_id = 1, category_id = 3;
-INSERT INTO lots SET creation_date = '2021-09-24 20:00', lot_name = 'Куртка для сноуборда DC Mutiny Charocal',
-	image = 'img/lot-5.jpg', start_price = 7500, exp_date = '2021-10-14 10:00', creator_id = 2, category_id = 4;
-INSERT INTO lots SET creation_date = '2021-09-24 17:00', lot_name = 'Маска Oakley Canopy',
-	image = 'img/lot-6.jpg', start_price = 5400, exp_date = '2021-10-14 18:00', creator_id = 2, category_id = 6;
-	
-CREATE INDEX lot_name ON lots(lot_name); # Для поиска лота по названию
+INSERT INTO users (reg_date, email, user_name, pass)
+VALUES
+	('2020-09-15 13:00', 'p.ivan@gmail.com', 'Иван Петров', 'qweqwe123'),
+	('2021-03-07 06:00', 'vasyliev1995@mail.ru', 'Сергей Васильев', 'qwerty12345');
 
-INSERT INTO users SET reg_date = '2020-09-15 13:00', email = 'p.ivan@gmail.com', user_name = 'Иван Петров', pass = 'qweqwe123';
-INSERT INTO users SET reg_date = '2021-03-07 06:00', email = 'vasyliev1995@mail.ru', user_name = 'Сергей Васильев', pass = 'qwerty12345';
+INSERT INTO lots (creation_date, lot_name, image, start_price, exp_date, creator_id, category_id)
+VALUES
+	('2021-09-27 00:00', '2014 Rossignol District Snowboard', 'img/lot-1.jpg', 10999, '2021-10-10 00:00', 1, 1),
+	('2021-09-27 03:00', 'DC Ply Mens 2016/2017 Snowboard', 'img/lot-2.jpg', 159999, '2021-10-11 00:00', 1, 1),
+	('2021-09-26 00:00', 'Крепления Union Contact Pro 2015 года размер L/XL', 'img/lot-3.jpg', 8000, '2021-10-12 15:00', 2, 2),
+	('2021-09-25 00:00', 'Ботинки для сноуборда DC Mutiny Charocal', 'img/lot-4.jpg', 10999, '2021-10-13 00:00', 1, 3),
+	('2021-09-24 20:00', 'Куртка для сноуборда DC Mutiny Charocal', 'img/lot-5.jpg', 7500, '2021-10-14 10:00', 2, 4),
+	('2021-09-24 17:00', 'Маска Oakley Canopy', 'img/lot-6.jpg', 5400, '2021-10-14 18:00', 2, 6);
 
-INSERT INTO bets SET bet_date = '2021-09-28 11:00', price = 12000, user_id = 1, lot_id = 1;
-INSERT INTO bets SET bet_date = '2021-09-29 07:00', price = 162000, user_id = 2, lot_id = 2;
+CREATE INDEX id ON lots(id); # Для поиска лота по идентификатору
+
+INSERT INTO bets (bet_date, price, user_id, lot_id)
+VALUES
+	('2021-09-28 11:00', 12000, 1, 1),
+	('2021-09-29 07:00', 162000, 2, 2),
+	('2021-09-28 22:00', 13000, 2, 1),
+	('2021-09-29 15:00', 165000, 1, 2);
+
 
 SELECT * FROM categories; # выбор всех категорий
 
 SELECT lot_name, start_price, image, COALESCE(MAX(b.price), start_price) AS price, category_name FROM lots l LEFT JOIN bets b ON l.id = b.lot_id 
-JOIN categories c ON l.category_id = c.id WHERE (TIMEDIFF(exp_date, NOW()) > 0) GROUP BY l.id; # выбор открытых лотов
+JOIN categories c ON l.category_id = c.id WHERE exp_date > NOW() GROUP BY l.id; # выбор открытых лотов
 
 SELECT l.id, creation_date, lot_name, description, image, start_price, exp_date, bet_range, creator_id, winner_id, category_id, category_name FROM lots l
 JOIN categories c ON l.category_id = c.id WHERE l.id = 5; # выбор лота по идентификатору
